07aeb8077b0741a44378b4478e1bd577ff5d41bc
William Mann <William Mann>
2017-02-25 18:58:46 +0000

    Changed the use of cookies in the program

--- src/php/createcookie.php
+++
@@ -1,9 +0,0 @@
-<?php
-	require "session.php";
-	$time = time()+ (60*60*24*30*12);
-	setcookie('signinuser', '',$time);
-	setcookie('signinhouse', '', $time);
-	setcookie('signinuser', 'viewsigninallowed',$time);
-	setcookie('signinhouse', $_GET["house"], $time);
-	header("location: ../view.php?house=".$_GET["house"]);
-?>
